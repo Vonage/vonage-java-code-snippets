12a951388e3a82bedd0de57013c956f1c6af0ba2
Steve Crow <steve.crow@vonage.com>
2018-05-02 18:14:18 -0400

    Update FROM_NUMBER to NEXMO_NUMBER to more consistently match specs.

--- src/main/java/com/nexmo/quickstart/voice/OutboundTextToSpeech.java
+++ src/main/java/com/nexmo/quickstart/voice/OutboundTextToSpeech.java
@@ -15,7 +15,7 @@ public class OutboundTextToSpeech {
 
         String NEXMO_APPLICATION_ID = envVar("APPLICATION_ID");
         String NEXMO_APPLICATION_PRIVATE_KEY = envVar("PRIVATE_KEY");
-        String NEXMO_NUMBER = envVar("FROM_NUMBER");
+        String NEXMO_NUMBER = envVar("NEXMO_NUMBER");
         String TO_NUMBER = envVar("TO_NUMBER");
 
         AuthMethod auth = new JWTAuthMethod(
