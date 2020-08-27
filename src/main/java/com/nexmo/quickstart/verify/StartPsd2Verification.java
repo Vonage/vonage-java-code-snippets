7ad9d566162b3852189b32a2aaed0e354e8a837d
Yvonne Allen <princess0115@gmail.com>
2020-08-27 11:28:45 -0400

    Add PSD2 code snippets.

--- src/main/java/com/nexmo/quickstart/verify/StartPsd2Verification.java
+++ src/main/java/com/nexmo/quickstart/verify/StartPsd2Verification.java
@@ -3,32 +3,28 @@ package com.nexmo.quickstart.verify;
 import com.nexmo.client.NexmoClient;
 import com.nexmo.client.verify.VerifyResponse;
 import com.nexmo.client.verify.VerifyStatus;
-import com.nexmo.client.verify.VerifyRequest;
 
 import static com.nexmo.quickstart.Util.configureLogging;
 import static com.nexmo.quickstart.Util.envVar;
 
-public class StartVerificationWithWorkflow {
+public class StartPsd2Verification {
     public static void main(String[] args) {
         configureLogging();
 
         String NEXMO_API_KEY = envVar("NEXMO_API_KEY");
         String NEXMO_API_SECRET = envVar("NEXMO_API_SECRET");
-
         String RECIPIENT_NUMBER = envVar("RECIPIENT_NUMBER");
-        String NEXMO_BRAND = envVar("BRAND_NAME");
+        String PAYEE = envVar("PAYEE");
+        double AMOUNT = Double.parseDouble(envVar("AMOUNT"));
 
-        VerifyRequest request = new VerifyRequest(RECIPIENT_NUMBER,NEXMO_BRAND);
-
-        request.setWorkflow(VerifyRequest.Workflow.TTS_TTS);
 
         NexmoClient client = NexmoClient.builder().apiKey(NEXMO_API_KEY).apiSecret(NEXMO_API_SECRET).build();
-        VerifyResponse response = client.getVerifyClient().verify(request);
+        VerifyResponse response = client.getVerifyClient().psd2Verify(RECIPIENT_NUMBER, AMOUNT, PAYEE);
 
         if (response.getStatus() == VerifyStatus.OK) {
-            System.out.printf("RequestID: %s", response.getRequestId());
+            System.out.printf("Request ID: %s", response.getRequestId());
         } else {
-            System.out.printf("ERROR! %s: %s", response.getStatus(), response.getErrorText());
+            System.out.printf("Error: %s: %s", response.getStatus(), response.getErrorText());
         }
     }
 }
--- src/main/java/com/nexmo/quickstart/verify/StartPsd2VerificationWithWorkflow.java
+++ src/main/java/com/nexmo/quickstart/verify/StartPsd2VerificationWithWorkflow.java
@@ -1,34 +1,34 @@
 package com.nexmo.quickstart.verify;
 
 import com.nexmo.client.NexmoClient;
+import com.nexmo.client.verify.BaseRequest;
+import com.nexmo.client.verify.Psd2Request;
 import com.nexmo.client.verify.VerifyResponse;
 import com.nexmo.client.verify.VerifyStatus;
-import com.nexmo.client.verify.VerifyRequest;
 
 import static com.nexmo.quickstart.Util.configureLogging;
 import static com.nexmo.quickstart.Util.envVar;
 
-public class StartVerificationWithWorkflow {
+public class StartPsd2VerificationWithWorkflow {
     public static void main(String[] args) {
         configureLogging();
 
         String NEXMO_API_KEY = envVar("NEXMO_API_KEY");
         String NEXMO_API_SECRET = envVar("NEXMO_API_SECRET");
-
         String RECIPIENT_NUMBER = envVar("RECIPIENT_NUMBER");
-        String NEXMO_BRAND = envVar("BRAND_NAME");
+        String PAYEE = envVar("PAYEE");
+        String AMOUNT = envVar("AMOUNT");
 
-        VerifyRequest request = new VerifyRequest(RECIPIENT_NUMBER,NEXMO_BRAND);
 
-        request.setWorkflow(VerifyRequest.Workflow.TTS_TTS);
 
         NexmoClient client = NexmoClient.builder().apiKey(NEXMO_API_KEY).apiSecret(NEXMO_API_SECRET).build();
-        VerifyResponse response = client.getVerifyClient().verify(request);
+        VerifyResponse response = client.getVerifyClient()
+                .psd2Verify(RECIPIENT_NUMBER, Double.parseDouble(AMOUNT), PAYEE, Psd2Request.Workflow.SMS_SMS);
 
         if (response.getStatus() == VerifyStatus.OK) {
-            System.out.printf("RequestID: %s", response.getRequestId());
+            System.out.printf("Request ID: %s", response.getRequestId());
         } else {
-            System.out.printf("ERROR! %s: %s", response.getStatus(), response.getErrorText());
+            System.out.printf("Error: %s: %s", response.getStatus(), response.getErrorText());
         }
     }
 }
