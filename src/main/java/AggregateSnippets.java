6983cd824746694c6eeb5115d48d39230901f292
Sina Madani <sina.madani@vonage.com>
2025-02-13 13:55:16 +0000

    Add copyright header to AggregateSnippets

--- src/main/java/AggregateSnippets.java
+++ src/main/java/AggregateSnippets.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2025 Vonage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -140,11 +162,12 @@ public final class AggregateSnippets {
 
             sb.append("\n```java\n").append(nugget).append("\n```\n");
 
+            boolean standalone = clientInitEndIndex < 12;
             snippetFiles.add(new CodeSnippetFile(path.toPath(),
                     lineNumberFromIndex(fileContent, startIndex) + 1,
                     lineNumberFromIndex(fileContent, endIndex),
-                    clientInitEndIndex < 12 ? -1 : lineNumberFromIndex(fileContent, clientInitStartIndex) + 1,
-                    clientInitEndIndex < 12 ? -1 : lineNumberFromIndex(fileContent, clientInitEndIndex)
+                    standalone ? -1 : lineNumberFromIndex(fileContent, clientInitStartIndex) + 1,
+                    standalone ? -1 : lineNumberFromIndex(fileContent, clientInitEndIndex)
             ));
         }
     }
