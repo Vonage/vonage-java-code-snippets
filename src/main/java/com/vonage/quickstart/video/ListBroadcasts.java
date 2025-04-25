5c5f6c7808b1f6b99d54f4fa70ff426d356d1e05
Sina Madani <sina.madani@vonage.com>
2025-04-25 14:12:07 +0100

    Update ListBroadcasts.java

--- src/main/java/com/vonage/quickstart/video/ListBroadcasts.java
+++ src/main/java/com/vonage/quickstart/video/ListBroadcasts.java
@@ -1,10 +1,23 @@
+/*
+ *   Copyright 2025 Vonage
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.vonage.quickstart.video;
 
 import com.vonage.client.VonageClient;
 import com.vonage.client.video.*;
 import static com.vonage.quickstart.EnvironmentVariables.*;
-import com.vonage.quickstart.util.JsonPrinter;
-
 import java.util.List;
 
 public class ListBroadcasts {
@@ -17,7 +30,6 @@ public class ListBroadcasts {
 
         List<Broadcast> broadcasts = videoClient.listBroadcasts();
 
-        System.out.println("Retrieved list of broadcasts: ");
-        JsonPrinter.print(broadcasts);
+        System.out.println("Retrieved list of broadcasts: " + broadcasts);
     }
 }
